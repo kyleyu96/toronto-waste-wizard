12e8dc344764be1b0523429646991fb6639e2b9b
KyleY <kyle.yu.96@outlook.com>
2019-01-18 14:03:17 -0500

    Updates

---
+++ precache-manifest.e9754e794f0d9a897c0db32239bdfa61.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e864a183eeb6e48c4a2",
+    "url": "/toronto-waste-wizard/static/js/runtime~main.8e864a18.js"
+  },
+  {
+    "revision": "85d0e5ed501f2b2c5710",
+    "url": "/toronto-waste-wizard/static/js/main.85d0e5ed.chunk.js"
+  },
+  {
+    "revision": "88f78307fb9230bb31a4",
+    "url": "/toronto-waste-wizard/static/js/1.88f78307.chunk.js"
+  },
+  {
+    "revision": "89d2eb0f12dbfd2d37829c7a81c58fe2",
+    "url": "/toronto-waste-wizard/index.html"
+  }
+];
